6902351ec1e5747ceaf67842e57e22f105e7c7c6
harrisonnad <harrisonn64@gmail.com>
2020-12-08 02:04:40 -0500

    FrontEnd Refactort

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -1,22 +1,21 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-import HomePage from './HomePage';
-import RoomJoinPage from './RoomJoinPage';
-import CreateRoomPage from './CreateRoomPage';
+import HomePage from "./HomePage";
 
 export default class App extends Component {
     constructor(props) {
         super(props);
     }
-    render(){
-        return (
-            <div>
-                <HomePage />
-                <RoomJoinPage />
-                <CreateRoomPage />
-            </div>)
+
+    render() {
+        return ( < div >
+            <
+            HomePage / >
+            <
+            /div>
+        );
     }
 }
 
 const appDiv = document.getElementById("app");
-render(<App />, appDiv);
+render( < App / > , appDiv);
